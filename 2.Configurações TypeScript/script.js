fa3ff08b9f7ef6decb660fd89cd1e3eb1a6bb659
Gabriel Silva Norberto <sn_gabriel@outlook.com>
2022-01-05 10:31:13 -0300

    Module - Finished

---
+++ 2.Configurações TypeScript/script.js
@@ -0,0 +1,5 @@
+var nome = 'Gabriel';
+function teste(n1) {
+    return 'O parâmetro era ' + n1;
+}
+teste(5);
---
+++ 2.Configurações TypeScript/script.ts
@@ -0,0 +1,7 @@
+const nome: string = 'Gabriel'
+
+function teste(n1:number){
+    return 'O parâmetro era '+ n1
+}
+
+teste(5)
